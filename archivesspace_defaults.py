51d281266e75cb9f481fb8ee0bb99eab173a5299
Walker Boyle <wboyle13@gmail.com>
2016-02-19 10:45:18 -0500

    add new enum values
    Added some code to set default values for the user defined enum 1 enumeration. 
    Also did a little bit of factoring out common code, and made print a function for python 3 compatibility.

--- archivesspace_defaults.py
+++ archivesspace_defaults.py
@@ -1,17 +1,18 @@
 import requests
 import time
 import json
+from pprint import pprint
 
 # This script is used to setup BHL ArchivesSpace defaults for running test migrations
 
 def test_connection():
 	try:
 		requests.get('http://localhost:8089')
-		print 'Connected!'
+		print('Connected!')
 		return True
 
 	except requests.exceptions.ConnectionError:
-		print 'Connection error. Trying again in 10 seconds.'
+		print('Connection error. Trying again in 10 seconds.')
 
 is_connected = test_connection()
 
@@ -31,7 +32,7 @@ bhl_repo = {
 		}
 
 post_repo = requests.post('http://localhost:8089/repositories',headers=headers,data=json.dumps(bhl_repo)).json()
-print post_repo
+print(post_repo)
 
 base_profile = {
 	'name':'',
@@ -48,24 +49,24 @@ for profile_name in profile_names:
 	container_profile = base_profile
 	container_profile['name'] = profile_name
 	profile_post = requests.post('http://localhost:8089/container_profiles',headers=headers,data=json.dumps(container_profile)).json()
-	print profile_post
+	print(profile_post)
 
 mhc_classification = {'title':'Michigan Historical Collections','identifier':'MHC'}
 uarp_classification = {'title':'University Archives and Records Program','identifier':'UARP'}
 
 for classification in [mhc_classification, uarp_classification]:
 	classification_post = requests.post('http://localhost:8089/repositories/2/classifications',headers=headers,data=json.dumps(classification)).json()
-	print classification_post
+	print(classification_post)
 
-subject_sources = requests.get('http://localhost:8089/config/enumerations/23',headers=headers).json()
-subject_sources['values'].extend(['lcnaf','lctgm','aacr2'])
-update_subject_sources = requests.post('http://localhost:8089/config/enumerations/23',headers=headers,data=json.dumps(subject_sources)).json()
-print update_subject_sources
-
-name_sources = requests.get('http://localhost:8089/config/enumerations/4',headers=headers).json()
-name_sources['values'].append('lcnaf')
-update_name_sources = requests.post('http://localhost:8089/config/enumerations/4',headers=headers,data=json.dumps(name_sources)).json()
-print update_name_sources
+def add_enum_values(enum_set_id, new_values_to_add):
+	enum_address = 'http://localhost:8089/config/enumerations/{}'.format(enum_set_id)
+	existing_enums_json = requests.get(enum_address, headers=headers).json()
+	existing_enums_json["values"].extend(new_values_to_add)
+	pprint(requests.post(enum_address, headers=headers, data=json.dumps(existing_enums_json)).json())
+	
+add_enum_values(23, ['lcnaf', 'lctgm', 'aacr2'])  # subject sources
+add_enum_values(4, ['lcnaf'])  # name sources
+add_enum_values(55, ["on file", "pending", "sent", "n/a", "other"])  # user defined enum 1 values (gift agreement status)
 
 repo_preferences = {
 	'repository':{'ref':'/repositories/2'},
@@ -73,4 +74,4 @@ repo_preferences = {
 	}
 
 repo_preferences_post = requests.post('http://localhost:8089/repositories/2/preferences',headers=headers, data=json.dumps(repo_preferences)).json()
-print repo_preferences_post
+print(repo_preferences_post)
