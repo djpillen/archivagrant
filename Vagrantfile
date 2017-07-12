686748c3580b60a6fc2c4dc06215c7547c8aaa01
djpillen@umich.edu <djpillen@umich.edu>
2017-07-12 15:22:03 -0400

    code cleanup

--- Vagrantfile
+++ Vagrantfile
@@ -11,15 +11,13 @@ Vagrant.configure(2) do |config|
   config.vm.network "forwarded_port", guest: 8080, host: 8080
   config.vm.network "forwarded_port", guest: 8081, host: 8081
   config.vm.network "forwarded_port", guest: 8089, host: 8089
+  config.vm.network "forwarded_port", guest: 8090, host: 8090
   config.vm.network "forwarded_port", guest: 8443, host: 8443
   config.vm.network "forwarded_port", guest: 8983, host: 8983
-  config.vm.network "forwarded_port", guest: 8090, host: 8090
 
   config.vm.provider "virtualbox" do |vb|
      vb.memory = "2048"
   end
-
-  config.vm.synced_folder "C:/Users/djpillen/GitHub", "/github"
 
   config.vm.provision "shell", path:"setup_python.sh"
   config.vm.provision "shell", path:"setup_mysql.sh"
--- setup_archivesspace.sh
+++ setup_archivesspace.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Installing dependencies"
 apt-get -y install openjdk-7-jdk
 apt-get -y install unzip
 apt-get -y install git
 apt-get -y install curl
 
-echo "Downloading latest ArchivesSpace release"
-# Use a Python script to download the latest ArchivesSpace release, because this is the only way that I know how
-#cd /vagrant
-#python download_latest_archivesspace.py
-
-# Sometimes I want to download a release candidate. Uncomment the below lines, add the direct link to the release candidate, and comment out the above python script
+# Download and unzip a packaged ArchivesSpace release and corresponding source code
 mkdir /aspace
 mkdir /aspace/source
 mkdir /aspace/zips
-#cd /aspace/source
-#git clone https://github.com/archivesspace/archivesspace.git
 cd /aspace/zips
-wget https://github.com/archivesspace/archivesspace/releases/download/v1.5.4/archivesspace-v1.5.4.zip
-wget https://github.com/archivesspace/archivesspace/archive/v1.5.4.zip
+wget -nv https://github.com/archivesspace/archivesspace/releases/download/v1.5.4/archivesspace-v1.5.4.zip
+wget -nv https://github.com/archivesspace/archivesspace/archive/v1.5.4.zip
 cd /aspace
 unzip /aspace/zips/archivesspace-v1.5.4.zip
 cd /aspace/source
@@ -43,9 +35,8 @@ PLUGINS="AppConfig[:plugins] = ['accession_events', 'aspace-jsonmodel-from-forma
 PUBLIC="AppConfig[:enable_public] = false"
 FRONTEND="AppConfig[:enable_frontend] = true"
 
-echo "Installing plugins"
+# Install plugins
 cd /aspace/archivesspace/plugins
-
 git clone https://github.com/bentley-historical-library/accession_events.git
 git clone https://github.com/bentley-historical-library/aspace-jsonmodel-from-format.git
 git clone https://github.com/bentley-historical-library/bhl_accession_readonly_fields.git
@@ -62,48 +53,41 @@ git clone https://github.com/bentley-historical-library/generate_bhl_identifiers
 git clone https://github.com/alexduryee/timewalk.git
 git clone https://github.com/bentley-historical-library/user_defined_in_basic.git
 
-echo "Installing mysql java connector"
 # http://archivesspace.github.io/archivesspace/user/running-archivesspace-against-mysql/
 cd /aspace/archivesspace/lib
-wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.39/mysql-connector-java-5.1.39.jar
+wget -nv http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.39/mysql-connector-java-5.1.39.jar
 
-echo "Editing config"
-cd /aspace/archivesspace/config
 
 # Edit the config file to use the MySQL database, setup our plugins, and disable the public and staff interfaces
 # http://stackoverflow.com/questions/14643531/changing-contents-of-a-file-through-shell-script
+cd /aspace/archivesspace/config
 sed -i "s@#AppConfig\[:db_url\].*@$DBURL@" config.rb
 sed -i "s@#AppConfig\[:plugins\].*@$PLUGINS@" config.rb
 sed -i "s@#AppConfig\[:enable_public\].*@$PUBLIC@" config.rb
 sed -i "s@#AppConfig\[:enable_frontend\].*@$FRONTEND@" config.rb
-
 echo $BROWSEURL >> config.rb
 cat /aspace/archivesspace/plugins/user_defined_in_basic/bhl_config.txt >> config.rb
 echo "" >> config.rb
 cat /aspace/archivesspace/plugins/accession_events/bhl_config.txt >> config.rb
 
-echo "Setting up database and starting ArchivesSpace"
-# First, make the setup-database.sh and archivesspace.sh scripts executable
+# Make the archivesspace.sh and setup-database.sh scripts executable
 cd /aspace/archivesspace/scripts
 chmod +x setup-database.sh
 cd /aspace/archivesspace
 chmod +x archivesspace.sh
 
-echo "Setting up database"
+# Run the setup database script
 scripts/setup-database.sh
 
-echo "Adding ArchivesSpace to system startup"
+# Add ArchivesSpace to system startup and create an ArchivesSpace service
 cd /etc/init.d
 ln -s /aspace/archivesspace/archivesspace.sh archivesspace
-
 update-rc.d archivesspace defaults
 update-rc.d archivesspace enable
 
+# Start ArchivesSpace
 cd /aspace/archivesspace
-
-echo "Starting ArchivesSpace"
 ./archivesspace.sh start
 
-echo "All done!"
-echo "Set up ArchivesSpace defaults (or import an ASpace mysql dump) and point your host machine's browser to http://localhost:8080 to begin using ArchivesSpace"
+echo "Browse to http://localhost:8080 to begin using the ArchivesSpace application"
 echo "Use vagrant ssh to access the virtual machine"
